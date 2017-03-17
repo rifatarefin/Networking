354adf6eed62618298165beb3a920d76709986e2
rifatarefin <rifatarefin19@gmail.com>
2017-03-17 15:04:09 +0600

    offline 1:v6

--- Scontroller/Scontrol1.java
+++ Scontroller/Scontrol1.java
@@ -4,6 +4,7 @@ package Scontroller;
  * Created by rifat on 3/17/17.
  */
 
+import Server.Server;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
@@ -15,6 +16,17 @@ import javafx.stage.FileChooser;
 import java.io.File;
 
 public class Scontrol1 {
+
+    Server server;
+    String dir;
+
+    public void setDir(String dir) {
+        this.dir = dir;
+    }
+
+    public void setServer(Server server) {
+        this.server = server;
+    }
 
     @FXML
     private TextField fileType;
@@ -35,13 +47,13 @@ public class Scontrol1 {
         constraints.setFileNum((int) fileNum.getValue());
         constraints.setMaxFileSize((int) maxFileSize.getValue());
         constraints.setIDRange(IDRange.getText());
+        constraints.setDestinationDir(dir);
 
     }
 
     @FXML
     void setDestination(ActionEvent event) {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Open Resource File");
+        server.filechoose();
 
 
     }
--- Server/Server.java
+++ Server/Server.java
@@ -3,16 +3,21 @@ package Server;/**
  */
 
 import Ccontroller.ClientController;
+import Scontroller.Scontrol1;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Server extends Application {
     Stage stage;
+    Scontrol1 scontrol1;
 
     public static void main(String[] args) {
         launch(args);
@@ -24,11 +29,23 @@ public class Server extends Application {
         FXMLLoader loader=new FXMLLoader();
         loader.setLocation(getClass().getResource("/serverPage1.fxml"));
         Parent root = loader.load();
-        //ClientController controller=loader.getController();
-        //controller.setMain(this);
+
+        scontrol1=loader.getController();
+        scontrol1.setServer(this);
         primaryStage.setScene(new Scene(root));
         stage=primaryStage;
         primaryStage.show();
 
     }
+    public void filechoose()
+    {
+        DirectoryChooser directoryChooser=new DirectoryChooser();
+        directoryChooser.setTitle("Open Destination");
+       // directoryChooser.setInitialDirectory(new File(""));
+        scontrol1.setDir(directoryChooser.showDialog(stage).getAbsolutePath());
+        System.out.println(directoryChooser.showDialog(stage).getAbsolutePath());
+
+
+    }
+
 }
